3ff22c68cba8845af124d717384dffc8f8d794f8
RyoTune <zutomayofan50@gmail.com>
2025-04-02 17:01:00 -0500

    Add FileSystem interface.

---
+++ RemixToolkit.Interfaces/IFileSystem.cs
@@ -0,0 +1,22 @@
+﻿namespace RemixToolkit.Interfaces;
+
+public interface IFileSystem
+{
+    void CopyFile(string sourceFile, string destFile);
+
+    void CopyFolder(string sourceDir, string destDir);
+
+    void CreateFolder(string dir);
+
+    void DeleteFile(string file);
+
+    void DeleteFolder(string dir);
+
+    bool FileExists(string file);
+
+    bool FolderExists(string dir);
+
+    string ReadFile(string file);
+
+    void WriteFile(string file, string content);
+}
