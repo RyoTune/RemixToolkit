474a5ddf7205b858d247a9ca635bb37e6fc218a3
RyoTune <zutomayofan50@gmail.com>
2025-04-01 23:01:01 -0500

    Add project files.

---
+++ RemixToolkit.Reloaded/Config.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using Reloaded.Mod.Interfaces.Structs;
+using RemixToolkit.Reloaded.Template.Configuration;
+
+namespace RemixToolkit.Reloaded.Configuration
+{
+    public class Config : Configurable<Config>
+    {
+    }
+
+    /// <summary>
+    /// Allows you to override certain aspects of the configuration creation process (e.g. create multiple configurations).
+    /// Override elements in <see cref="ConfiguratorMixinBase"/> for finer control.
+    /// </summary>
+    public class ConfiguratorMixin : ConfiguratorMixinBase
+    {
+    }
+}
